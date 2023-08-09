2af5b4cb1fd01e5f5094d53ee7ed0a4e1680e945
Renchester Ramos <renchesterjramos@gmail.com>
2023-08-09 22:58:42 +0800

    Update user schema

--- src/schemas/UserSchema.ts
+++ src/schemas/UserSchema.ts
@@ -3,10 +3,7 @@ import { z } from 'zod';
 const userSchema = z.object({
 	id: z.string(),
 	name: z.string(),
-	rank: z
-		.string()
-		.transform((v) => parseInt(v))
-		.refine((v) => !isNaN(v), { message: 'Not a number' }),
+	rank: z.number(),
 });
 
 export type User = z.infer<typeof userSchema>;
